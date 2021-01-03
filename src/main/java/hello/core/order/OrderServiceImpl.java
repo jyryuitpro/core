c24be8e2200725de9b9093f9663504c6175b2410
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-03 16:07:06 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -25,4 +25,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName, itemPirce, discountPrice);
     }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
