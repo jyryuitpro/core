56b0bc124af1e8f9a1843466768b8d9bd35d109f
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-05 08:47:10 +0900

    중복 등록과 충돌

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,13 +5,12 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Component  //memoryMemberRepository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
 
     @Override
-
     public void save(Member member) {
         store.put(member.getId(), member);
     }
